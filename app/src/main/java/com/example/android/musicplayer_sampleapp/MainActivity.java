dbad111b052a32d46dda5350a91985cba93c4ff8
Carlos <cperalt06@citymail.cuny.edu>
2018-01-27 18:45:33 -0500

    Renamed MP3s to lower case and Added Play and Pause Playback

--- app/src/main/java/com/example/android/musicplayer_sampleapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer_sampleapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayer_sampleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.seth_power_fire);
+
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
     }
 }
